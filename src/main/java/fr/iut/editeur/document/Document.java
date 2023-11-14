10414f5046f282170b776fe73de3aa5300736201
gillet <paco.gillet@etu-umontpellier.fr>
2023-11-14 16:54:37 +0100

    doc: documentation de la classe Document

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,13 +1,22 @@
 package fr.iut.editeur.document;
 
+/**
+ * Classe simulant un document contenant du texte
+ */
 public class Document {
-
+    /**
+     * Texte du Document
+     */
     private String texte;
 
     public Document() {
         this.texte = "";
     }
-	
+
+    /**
+     * Renvoie le texte actuel
+     * @return le texte actuel
+     */
     public String getTexte() {
         return texte;
     }
@@ -20,12 +29,23 @@ public class Document {
         this.texte += texte;
     }
 
+    /**
+     * Remplace le texte actuel de {@param start} à {@param end} par {@param remplacement}
+     * @param start Position de début
+     * @param end Position de fin
+     * @param remplacement Texte qui sera mis à la place de l'actuel
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Met en majuscule le texte de {@param start} à {@param end} en utilisant la méthode {@link #remplacer}
+     * @param start Position de début
+     * @param end Position de fin
+     */
     public void majuscules(int start, int end) {
         remplacer(start, end, texte.substring(start, end).toUpperCase());
     }
